a180fe104dd027bcadfe9766ee466cbf54a74bfd
Andrei Kortunov <andrei.kortunov@yandex.ru>
2019-09-16 14:18:41 +0400

    Do not allow player to take items from evidence chests (bug #3609)

---
+++ components/widgets/imagebutton.cpp
@@ -0,0 +1,124 @@
+#include "imagebutton.hpp"
+
+#include <MyGUI_RenderManager.h>
+
+#include <components/debug/debuglog.hpp>
+
+namespace Gui
+{
+
+    bool ImageButton::sDefaultNeedKeyFocus = true;
+
+    ImageButton::ImageButton()
+        : Base()
+        , mMouseFocus(false)
+        , mMousePress(false)
+        , mKeyFocus(false)
+    {
+        setNeedKeyFocus(sDefaultNeedKeyFocus);
+    }
+
+    void ImageButton::setDefaultNeedKeyFocus(bool enabled)
+    {
+        sDefaultNeedKeyFocus = enabled;
+    }
+
+    void ImageButton::setPropertyOverride(const std::string &_key, const std::string &_value)
+    {
+        if (_key == "ImageHighlighted")
+            mImageHighlighted = _value;
+        else if (_key == "ImagePushed")
+            mImagePushed = _value;
+        else if (_key == "ImageNormal")
+        {
+            if (mImageNormal == "")
+            {
+                setImageTexture(_value);
+            }
+            mImageNormal = _value;
+        }
+        else
+            ImageBox::setPropertyOverride(_key, _value);
+    }
+    void ImageButton::onMouseSetFocus(Widget* _old)
+    {
+        mMouseFocus = true;
+        updateImage();
+        Base::onMouseSetFocus(_old);
+    }
+
+    void ImageButton::onMouseLostFocus(Widget* _new)
+    {
+        mMouseFocus = false;
+        updateImage();
+        Base::onMouseLostFocus(_new);
+    }
+
+    void ImageButton::onMouseButtonPressed(int _left, int _top, MyGUI::MouseButton _id)
+    {
+        if (_id == MyGUI::MouseButton::Left)
+        {
+            mMousePress = true;
+            updateImage();
+        }
+        Base::onMouseButtonPressed(_left, _top, _id);
+    }
+
+    void ImageButton::updateImage()
+    {
+        if (mMousePress)
+            setImageTexture(mImagePushed);
+        else if (mMouseFocus || mKeyFocus)
+            setImageTexture(mImageHighlighted);
+        else
+            setImageTexture(mImageNormal);
+    }
+
+    MyGUI::IntSize ImageButton::getRequestedSize()
+    {
+        MyGUI::ITexture* texture = MyGUI::RenderManager::getInstance().getTexture(mImageNormal);
+        if (!texture)
+        {
+            Log(Debug::Error) << "ImageButton: can't find image " << mImageNormal;
+            return MyGUI::IntSize(0,0);
+        }
+        return MyGUI::IntSize (texture->getWidth(), texture->getHeight());
+    }
+
+    void ImageButton::setImage(const std::string &image)
+    {
+        size_t extpos = image.find_last_of(".");
+        std::string imageNoExt = image.substr(0, extpos);
+
+        std::string ext = image.substr(extpos);
+
+        mImageNormal = imageNoExt + "_idle" + ext;
+        mImageHighlighted = imageNoExt + "_over" + ext;
+        mImagePushed = imageNoExt + "_pressed" + ext;
+
+        setImageTexture(mImageNormal);
+    }
+
+    void ImageButton::onMouseButtonReleased(int _left, int _top, MyGUI::MouseButton _id)
+    {
+        if (_id == MyGUI::MouseButton::Left)
+        {
+            mMousePress = false;
+            updateImage();
+        }
+
+        Base::onMouseButtonReleased(_left, _top, _id);
+    }
+
+    void ImageButton::onKeySetFocus(MyGUI::Widget *_old)
+    {
+        mKeyFocus = true;
+        updateImage();
+    }
+
+    void ImageButton::onKeyLostFocus(MyGUI::Widget *_new)
+    {
+        mKeyFocus = false;
+        updateImage();
+    }
+}
