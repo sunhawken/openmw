3a0acf01a9ec3b9f0bdafd777a18a138ea07e8cf
Sam Hellawell <sshellawell@gmail.com>
2024-03-03 20:18:33 +0000

    Fix scaled shapes in navmesh generation

--- components/physicshelpers/aabb.hpp
+++ components/physicshelpers/aabb.hpp
@@ -10,14 +10,24 @@
 
 namespace PhysicsSystemHelpers
 {
-    inline JPH::AABox getAabb(const JPH::Shape& shape, const JPH::RMat44& transform)
+    inline JPH::AABox getAabb(const JPH::Shape* shape, const JPH::RMat44& transform)
     {
         const JPH::Vec3 inScale = JPH::Vec3(1.0f, 1.0f, 1.0f);
-        JPH::AABox bounds = shape.GetWorldSpaceBounds(transform, inScale);
+        JPH::AABox bounds = shape->GetWorldSpaceBounds(transform, inScale);
         return bounds;
     }
 
+    inline JPH::AABox getAabb(const JPH::Shape& shape, const JPH::RMat44& transform)
+    {
+        return getAabb(&shape, transform);
+    }
+
     inline JPH::AABox getAabb(const JPH::Shape& shape, const osg::Matrixd& transform)
+    {
+        return getAabb(&shape, Misc::Convert::toJoltNoScale(transform));
+    }
+
+    inline JPH::AABox getAabb(const JPH::Shape* shape, const osg::Matrixd& transform)
     {
         return getAabb(shape, Misc::Convert::toJoltNoScale(transform));
     }
