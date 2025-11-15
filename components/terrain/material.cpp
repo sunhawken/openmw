d18047c004992b0d5ae8bc7ce65aa5cd989eca27
Dirtybioleo <metzook@gmail.com>
2025-11-15 23:09:05 +0700

    very borked

--- components/terrain/material.cpp
+++ components/terrain/material.cpp
@@ -307,6 +307,9 @@ namespace Terrain
                 defineMap["reconstructNormalZ"] = reconstructNormalZ ? "1" : "0";
                 // Enable snow deformation shader code
                 defineMap["snowDeformation"] = "1";
+                // Ensure all required defines are set (some are global but need defaults)
+                defineMap["forcePPL"] = "0";  // Will be overridden by global if needed
+                defineMap["shadows_enabled"] = "0";  // Will be overridden if shadows enabled
                 Stereo::shaderStereoDefines(defineMap);
 
                 stateset->setAttributeAndModes(shaderManager.getProgram("terrain", defineMap));
