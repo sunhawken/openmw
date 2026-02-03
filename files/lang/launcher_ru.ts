8f999a80afcdcaf15421af517648710ad991a566
AnyOldName3 <krizdjali@gmail.com>
2026-02-03 23:46:40 +0000

    Switch to QTreeWidget

--- files/lang/launcher_ru.ts
+++ files/lang/launcher_ru.ts
@@ -628,22 +628,6 @@ to default Morrowind fonts. Check this box if you still prefer original fonts ov
         <source>Open settings.cfg</source>
         <translation type="unfinished"></translation>
     </message>
-    <message>
-        <source>&lt;b&gt;%1&lt;/b&gt; contains an &lt;b&gt;openmw.cfg&lt;/b&gt; and &lt;b&gt;settings.cfg&lt;/b&gt;</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>&lt;b&gt;%1&lt;/b&gt; contains an &lt;b&gt;openmw.cfg&lt;/b&gt;</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>&lt;b&gt;%1&lt;/b&gt; contains a &lt;b&gt;settings.cfg&lt;/b&gt;</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>&lt;b&gt;%1&lt;/b&gt; doesn&apos;t have an &lt;b&gt;openmw.cfg&lt;/b&gt; or &lt;b&gt;settings.cfg&lt;/b&gt;</source>
-        <translation type="unfinished"></translation>
-    </message>
 </context>
 <context>
     <name>MainWindow</name>
--- files/lang/launcher_sv.ts
+++ files/lang/launcher_sv.ts
@@ -601,22 +601,6 @@ de ordinarie fonterna i Morrowind. Bocka denna ruta om du ändå föredrar ordin
         <source>Open settings.cfg</source>
         <translation type="unfinished"></translation>
     </message>
-    <message>
-        <source>&lt;b&gt;%1&lt;/b&gt; contains an &lt;b&gt;openmw.cfg&lt;/b&gt; and &lt;b&gt;settings.cfg&lt;/b&gt;</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>&lt;b&gt;%1&lt;/b&gt; contains an &lt;b&gt;openmw.cfg&lt;/b&gt;</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>&lt;b&gt;%1&lt;/b&gt; contains a &lt;b&gt;settings.cfg&lt;/b&gt;</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>&lt;b&gt;%1&lt;/b&gt; doesn&apos;t have an &lt;b&gt;openmw.cfg&lt;/b&gt; or &lt;b&gt;settings.cfg&lt;/b&gt;</source>
-        <translation type="unfinished"></translation>
-    </message>
 </context>
 <context>
     <name>MainWindow</name>
