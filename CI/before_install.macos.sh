ecc35002138d4af735bc2bde2fce322a7581d9b0
Sarah Sunday <1644563-ssunday@users.noreply.gitlab.com>
2026-02-25 13:42:20 -0600

    [MAC] Use main branch, deps key, osx target

--- CI/before_install.macos.sh
+++ CI/before_install.macos.sh
@@ -1,11 +1,11 @@
 #!/bin/sh -ex
 
+source ./CI/macos/deps_versions.sh
+
 brew tap --repair
 brew update --quiet
 
 brew install curl p7zip
-
-VCPKG_TAG="2026-02-24"
 
 if [[ "${MACOS_AMD64}" ]]; then
     VCPKG_FILE="vcpkg-x64-osx-dynamic"
@@ -20,13 +20,13 @@ else
     brew install fontconfig yaml-cpp sqlite
 fi
 
-curl "https://gitlab.com/OpenMW/openmw-deps/-/raw/vcpkg-8306cb2556b92c9bb891b13f4769889838fe3c10/macos/${VCPKG_FILE}-${VCPKG_TAG}-manifest.txt" -o openmw-manifest.txt
+curl "https://gitlab.com/OpenMW/openmw-deps/-/raw/main/macos/${VCPKG_FILE}-${VCPKG_TAG}-manifest.txt" -o openmw-manifest.txt
 
 { read -r URL && read -r HASH FILE; } < openmw-manifest.txt
 
 curl -fSL -R -J $URL -o $FILE
 echo "${HASH:?}  ${FILE:?}" | sha512sum
-7z x -y -o/tmp/openmw-deps-pre -spe $FILE && \
+7z x -y -o/tmp/openmw-deps-pre $FILE && \
     mv /tmp/openmw-deps-pre/*/ /tmp/openmw-deps/ && \
     rmdir /tmp/openmw-deps-pre
 
